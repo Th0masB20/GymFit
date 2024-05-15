c464db32eb88df644419bf449fa6354059b1bfe3
Thommy Baez <thommybaez93@gmail.com>
2024-05-15 18:46:51 -0400

    Added workout card able to add sets and add the number of reps and weights for each sets able to add the workout days of the workout

--- frontend/src/component/CreateWorkoutPage.tsx
+++ frontend/src/component/CreateWorkoutPage.tsx
@@ -4,12 +4,12 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ExerciseRequestData } from "../interfaces/ICacheExercises";
 
-const CreateWorkout = (): React.ReactElement => {
+const CreateWorkoutPage = (): React.ReactElement => {
   const [user, setUser] = useState<IUser>();
   const [workoutName, setWorkoutName] = useState<string>("");
   const [cachedExercises, populateCache] = useState<ExerciseRequestData[]>();
   const [exercises, setWorkoutExercises] = useState<IExercise[]>([]);
-  const [workoutDays, setWorkoutDays] = useState<string[]>();
+  const [workoutDays, setWorkoutDays] = useState<Set<string>>(new Set());
 
   const [displayExerciseSearch, showSearch] = useState<boolean>(false);
 
@@ -44,7 +44,6 @@ const CreateWorkout = (): React.ReactElement => {
         if (cachedExercises == undefined) {
           const response = await axios.request(options);
           populateCache(response.data);
-          console.log(response.data);
         }
       } catch (error) {
         console.error(error);
@@ -98,17 +97,28 @@ const CreateWorkout = (): React.ReactElement => {
       <div className="w-full h-auto flex justify-center items-center flex-wrap">
         {exercises.map((currentExercise, i) => {
           return (
-            <DisplayedExercise currentExercise={currentExercise} key={i} />
+            <DisplayedExercise
+              currentExercise={currentExercise}
+              exerciseIndex={i}
+              setWorkoutExercises={setWorkoutExercises}
+              key={i + 1000}
+            />
           );
         })}
       </div>
-      <div className="fixed bg-[rgba(255,255,255,0.7)] w-full h-20 flex justify-center items-center bottom-0">
-        <button className="w-52 h-10 bg-second rounded-lg hover:scale-110 mr-2">
-          Delete Workout
-        </button>
-        <button className="w-52 h-10 bg-main rounded-lg hover:scale-110 ml-2">
-          Save Workout
-        </button>
+      <div className="fixed bottom-0 w-full flex flex-col justify-center items-center">
+        <SetWorkoutDays
+          setWorkoutDays={setWorkoutDays}
+          workoutDays={workoutDays}
+        />
+        <div className="bg-[rgba(255,255,255,0.7)] w-full h-20 flex justify-center items-center">
+          <button className="w-52 h-10 bg-second rounded-lg hover:scale-110 mr-2">
+            Delete Workout
+          </button>
+          <button className="w-52 h-10 bg-main rounded-lg hover:scale-110 ml-2">
+            Save Workout
+          </button>
+        </div>
       </div>
       {displayExerciseSearch && cachedExercises ? (
         <WorkoutSearch
@@ -118,6 +128,218 @@ const CreateWorkout = (): React.ReactElement => {
         />
       ) : null}
     </main>
+  );
+};
+
+const SetWorkoutDays = ({
+  setWorkoutDays,
+  workoutDays,
+}: {
+  setWorkoutDays: React.Dispatch<React.SetStateAction<Set<string>>>;
+  workoutDays: Set<string>;
+}): React.ReactElement => {
+  const [selected, setSelected] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const setRemoveDay = (dayIndex: number) => {
+    switch (dayIndex) {
+      case 0: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Monday")) {
+          newSet.delete("Monday");
+          const newArr = selected.map((val, i) => (i == 0 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Monday");
+          const newArr = selected.map((val, i) => (i == 0 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+
+      case 1: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Tuesday")) {
+          newSet.delete("Tuesday");
+          const newArr = selected.map((val, i) => (i == 1 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Tuesday");
+          const newArr = selected.map((val, i) => (i == 1 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+      case 2: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Wednesday")) {
+          newSet.delete("Wednesday");
+          const newArr = selected.map((val, i) => (i == 2 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Wednesday");
+          const newArr = selected.map((val, i) => (i == 2 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+      case 3: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Thursday")) {
+          newSet.delete("Thursday");
+          const newArr = selected.map((val, i) => (i == 3 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Thursday");
+          const newArr = selected.map((val, i) => (i == 3 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+
+      case 4: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Friday")) {
+          newSet.delete("Friday");
+          const newArr = selected.map((val, i) => (i == 4 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Friday");
+          const newArr = selected.map((val, i) => (i == 4 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+
+      case 5: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Saturday")) {
+          newSet.delete("Saturday");
+          const newArr = selected.map((val, i) => (i == 5 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Saturday");
+          const newArr = selected.map((val, i) => (i == 5 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+      case 6: {
+        const newSet = new Set<string>(workoutDays);
+        if (newSet.has("Sunday")) {
+          newSet.delete("Sunday");
+          const newArr = selected.map((val, i) => (i == 6 ? false : val));
+          setSelected(newArr);
+        } else {
+          newSet.add("Sunday");
+          const newArr = selected.map((val, i) => (i == 6 ? true : val));
+          setSelected(newArr);
+        }
+        setWorkoutDays(newSet);
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-80 h-20">
+      <button className="bg-main w-24 h-7 mb-2 rounded-lg hover:scale-110">
+        Set Days
+      </button>
+      <div className="w-full h-9 bg-second rounded-lg flex items-center justify-center">
+        <ul className="inline">
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[0] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(0)}
+            >
+              M
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[1] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(1)}
+            >
+              T
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[2] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(2)}
+            >
+              W
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[3] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(3)}
+            >
+              TH
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[4] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(4)}
+            >
+              F
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[5] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(5)}
+            >
+              S
+            </p>
+          </li>
+          <li className="inline-block">
+            <p
+              className={
+                "hover:cursor-pointer hover:scale-110 text-lg text-center w-8 h-7 mx-1 " +
+                (selected[6] ? "bg-main rounded-full" : "")
+              }
+              onClick={() => setRemoveDay(6)}
+            >
+              S
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
@@ -148,7 +370,7 @@ const WorkoutSearch = ({
             exercise={exercise}
             setWorkoutExercises={setWorkoutExercises}
             showSearch={showSearch}
-            key={i}
+            key={i + 10000000}
           />
         );
       });
@@ -163,7 +385,7 @@ const WorkoutSearch = ({
             exercise={exercise}
             setWorkoutExercises={setWorkoutExercises}
             showSearch={showSearch}
-            key={i}
+            key={i + 100000000}
           />
         );
       }
@@ -239,8 +461,12 @@ const ExerciseComponent = ({
 
 const DisplayedExercise = ({
   currentExercise,
+  setWorkoutExercises,
+  exerciseIndex,
 }: {
   currentExercise: IExercise;
+  exerciseIndex: number;
+  setWorkoutExercises: React.Dispatch<React.SetStateAction<IExercise[]>>;
 }): React.ReactElement => {
   const [setArray, addToSetArry] = useState<React.ReactElement[]>();
 
@@ -248,14 +474,20 @@ const DisplayedExercise = ({
     currentExercise.numberOfSets++;
     const setArray: React.ReactElement[] = [];
     for (let i = 0; i < currentExercise.numberOfSets; i++) {
-      setArray.push(<SetRepWeightComponent setIndex={i} />);
+      setArray.push(
+        <SetRepWeightComponent
+          setIndex={i}
+          exerciseIndex={exerciseIndex}
+          setWorkoutExercises={setWorkoutExercises}
+          key={i + 100000}
+        />
+      );
     }
-
     addToSetArry(setArray);
   };
 
   return (
-    <div className="w-80 min-h-72 h-auto bg-main m-10 flex flex-col justify-around items-center rounded-3xl">
+    <div className="w-80 min-h-72 h-auto bg-fourth m-10 flex flex-col justify-around items-center rounded-3xl">
       <p className="text-center text-xl mb-3 mt-3">
         {currentExercise.exerciseName}
       </p>
@@ -277,20 +509,74 @@ const DisplayedExercise = ({
 
 const SetRepWeightComponent = ({
   setIndex,
+  exerciseIndex,
+  setWorkoutExercises,
 }: {
   setIndex: number;
+  exerciseIndex: number;
+  setWorkoutExercises: React.Dispatch<React.SetStateAction<IExercise[]>>;
 }): React.ReactElement => {
+  const [repNumber, setRepNumber] = useState<number>(0);
+  const [weightNumber, setWeightNumber] = useState<number>(0);
+
+  const onRepNumberInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    if (isNaN(value)) return;
+    setRepNumber(Number(value));
+  };
+
+  const onWeightNumberInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    if (isNaN(value)) return;
+    setWeightNumber(Number(value));
+  };
+
+  useEffect(() => {
+    if (weightNumber && repNumber) {
+      setWorkoutExercises((current) => {
+        const newExerciseArray = current.map((currentExercise, i) => {
+          const newRepArray = [...currentExercise.reps];
+          const newWeightArray = [...currentExercise.weights];
+
+          if (
+            setIndex > newRepArray.length ||
+            setIndex > newWeightArray.length
+          ) {
+            newRepArray.push(repNumber);
+            newWeightArray.push(weightNumber);
+          } else {
+            newRepArray[setIndex] = repNumber;
+            newWeightArray[setIndex] = weightNumber;
+          }
+          return i == exerciseIndex
+            ? { ...currentExercise, reps: newRepArray, weights: newWeightArray }
+            : currentExercise;
+        });
+
+        return newExerciseArray;
+      });
+    }
+  }, [exerciseIndex, repNumber, setIndex, setWorkoutExercises, weightNumber]);
+
   return (
     <>
       <div className="flex w-52 justify-around mb-2">
         <div className="w-5 h-6 rounded-full bg-soft-3 text-center">
-          {setIndex}
+          {setIndex + 1}
         </div>
-        <input className="w-14 rounded-full bg-soft-1 text-center" />
-        <input className="w-14 rounded-full bg-soft-1 text-center" />
+        <input
+          className="w-14 rounded-full bg-soft-1 text-center"
+          onChange={onRepNumberInput}
+          value={repNumber}
+        />
+        <input
+          className="w-14 rounded-full bg-soft-1 text-center"
+          onChange={onWeightNumberInput}
+          value={weightNumber}
+        />
       </div>
     </>
   );
 };
 
-export default CreateWorkout;
+export default CreateWorkoutPage;
