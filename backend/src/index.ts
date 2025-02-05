6f5a166f8cd627a74449a37e146b2590e397c446
Th0masB20 <thommybaez93@gmail.com>
2025-02-04 23:49:55 -0500

    added https

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import https from "https"
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import fs from 'fs'
 import cookieParser from 'cookie-parser';
 import registerRouter from "./routes/RegisterRoute";
 import ErrorHandler from "./utilities/ErrorHandler";
@@ -18,13 +20,14 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('./dist'));
 
 const connectionString: string = `${process.env.CONNECTION}${process.env.PASSWORD}${process.env.CONNECTION_END}`
 const PORT: number = Number(process.env.PORT) || 3000;
+const certificates: https.ServerOptions = { key: fs.readFileSync('./certificates/key.pem'), cert: fs.readFileSync('certificates/cert.pem') }
+const server = https.createServer(certificates, app);
 
 mongoose.connect(connectionString).then(() => {
-    app.listen(PORT, () => console.log('running on port ' + PORT));
+    server.listen(PORT, () => console.log('running on port ' + PORT));
 }
 ).catch(
     (error) => {
